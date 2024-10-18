9eb2255374662f483251cadce3cc548caaa34088
Cirovsky <cirovisqui@gmail.com>
2024-10-18 17:47:43 -0300

    mundanças na lógica do pulo duplo, para evitar usar dois pulos durante uma queda.

--- character/character.gd
+++ character/character.gd
@@ -41,12 +41,18 @@ func _vertical_moviment(_delta:float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * _delta
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and _extra_jump >= 0:
+	if Input.is_action_just_pressed("jump") and _extra_jump >= 0 and is_on_floor():
 		if Input.is_action_pressed("boost"):
 				velocity.y = _jump_velocity * 1.1
 		else:
 			velocity.y = _jump_velocity
 		_extra_jump -= 1
+	elif Input.is_action_just_pressed("jump") and _extra_jump >= 0:
+		if Input.is_action_pressed("boost"):
+				velocity.y = _jump_velocity * 1.1
+		else:
+			velocity.y = _jump_velocity
+		_extra_jump -= 2
 			
 func _horizontal_moviment(_delta:float) -> void:
 	_direction = Input.get_axis("move_left", "move_right")
