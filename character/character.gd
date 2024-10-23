ef6922fa3671e9a5e97c78e38ddf676b23446067
Cirovsky <cirovisqui@gmail.com>
2024-10-23 16:27:39 -0300

    correção de bug do ataque pular depois de realizado o segundo ataque no chão

--- character/character.gd
+++ character/character.gd
@@ -46,6 +46,7 @@ func _vertical_moviment(_delta:float) -> void:
 		velocity += get_gravity() * _delta
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and _extra_jump >= 0 and is_on_floor():
+		_attack_index = 1
 		global.spam_effect(
 			"res://visual_effects/dust_particles/jump/jump_effect.tscn",
 			Vector2(0, 2),
